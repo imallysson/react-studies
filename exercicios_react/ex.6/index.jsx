6f0629e52860a3d0b69ace7ef0ef8ac39e3767dc
Allysson Matheus dos Santos <allysson.lp@gmail.com>
2018-12-26 23:21:47 -0200

    section three.

---
+++ exercicios_react/ex.6/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Family from './family'
+import Member from './member'
+
+ReactDOM.render(
+    <Family>
+        <Member name="Guilherme" lastName="Silva" />
+    </Family>
+    , document.getElementById('app'))
