452448a8b3a857baaf7819a443b1a2fcc9b3eaad
Allysson Matheus dos Santos <allysson.lp@gmail.com>
2019-02-14 21:09:59 -0200

    section eleven.

--- my-money-app/backend/src/config/Routes.js
+++ my-money-app/backend/src/config/Routes.js
@@ -1,10 +1,22 @@
 const express = require('express')
+const auth = require('./auth')
 
-module.exports = function(server) {
+module.exports = function (server) {
 
-    const router = express.Router()
-    server.use('/api', router)
+    // Rotas protegidas por Token JWT
+    const protectedApi = express.Router()
+    server.use('/api', protectedApi)
+    protectedApi.use(auth)
 
     const BillingCycle = require('../api/billingCycle/billingCycleService')
-    BillingCycle.register(router, '/billingCycles')
+    BillingCycle.register(protectedApi, '/billingCycles')
+
+    // Rotas abertas
+    const openApi = express.Router()
+    server.use('/oapi', openApi)
+
+    const AuthService = require('../api/user/AuthService')
+    openApi.post('/login', AuthService.login)
+    openApi.post('/signup', AuthService.signup)
+    openApi.post('/validateToken', AuthService.validateToken)
 }
