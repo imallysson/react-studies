6730c4b3fc85092437b0591ea99c5dc8bba19a3e
Allysson Matheus dos Santos <allysson.lp@gmail.com>
2019-02-26 22:53:41 -0300

    section twenty-two.

---
+++ usuarios/frontend/src/components/template/Footer.jsx
@@ -0,0 +1,8 @@
+import './Footer.css'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        Desenvolvido com &nbsp;<i className="fa fa-heart text-danger"></i>&nbsp; por
+        <strong>&nbsp;Jiraya <span className="text-danger">Sensei</span></strong>
+    </footer>
